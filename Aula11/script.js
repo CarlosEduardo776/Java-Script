fecf2db2ebcf3933bb42fdf773f6b1b75f62bc78
Carlos Eduardo da Silva Santos <carloseduardodass76@gmail.com>
2024-06-11 20:35:02 -0300

    aa

---
+++ Aula11/script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector ('button');
+
+const cores = document.querySelector ('select');
+
+function mudarCor() {
+    botao.style.backgroundColor = cores.value;
+    
+}
